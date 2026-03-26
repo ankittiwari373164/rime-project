b6fb5b1219205d2bacf3c73d442f3af2972ed997
saquib39 <mohdsaquib619@gmail.com>
2026-03-27 02:13:46 +0530

    error fix

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ const PORT = process.env.PORT;
 
 connectDB();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://rime.co.in");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 // ✅ CORS sabse pehle
 app.use(cors({
   origin: "https://rime.co.in",
