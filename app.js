8df22fd962b814899db5037b3099afba12fbe133
saquib39 <mohdsaquib619@gmail.com>
2026-03-27 02:19:42 +0530

    fix

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://rime.co.in");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "https://rime.co.in");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+    return res.status(200).end();
   }
 
   next();
@@ -37,7 +37,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", cors());
+// app.options("*", cors());
 
 // ✅ body & cookies
 app.use(express.json());
