fb1700fc403c79946a554a0cea3e549fc7536373
saquib39 <mohdsaquib619@gmail.com>
2026-04-16 23:20:08 +0530

    added google sheet

--- src/controllers/applicationControllers.js
+++ src/controllers/applicationControllers.js
@@ -45,7 +45,7 @@ export const createApplication = async (req, res) => {
     const populatedApp = await Application.findById(application._id).populate("course");
 
     // 3. 🔥 Forward to Google Sheets (Silent Background Request)
-    const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbyxxZVs6YBVUulrppQ8O96zJ7LkFqpb_gp7uoabzwRTJ6zVhbYAnC20KE8PF__nwAOC/exec";
+    const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbw0U4QhCJm7-W1Jfsy2Kl-Z9NLtPN4MLZvE7jz1Gb6ANuG1SbRziK-dp4Ni9yvBNFY/exec";
 
     fetch(GOOGLE_SHEET_URL, {
       method: "POST",
